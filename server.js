2237c629839d34aba67df0a530070192d88ea609
Sharan Sajiv Menon <sharansajivmenon@gmail.com>
2018-01-08 13:49:59 -0500

    Setup start script

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
-const fs = require('fs')
+const fs = require('fs');
+const port  = process.env.PORT || 3000;
 var app = express();
 hbs.registerPartials(__dirname+'/views/partials');
 app.set('view engine','hbs')
@@ -15,9 +16,9 @@ app.use((req,res,next) => {
     });
     next();
 });
-app.use((req,res,next) => {
-    res.render('maintenance.hbs')
-});
+// app.use((req,res,next) => {
+//     res.render('maintenance.hbs')
+// });
 app.use(express.static(__dirname+'/public'));
 hbs.registerHelper('getCurrentYear', () => {
     return new Date().getFullYear();
@@ -37,6 +38,6 @@ app.get('/about', (req, res) => {
         pageTitle:'About',
     });
 })
-app.listen(3000, () => {
-    console.log('server is ready')
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}`)
 });
